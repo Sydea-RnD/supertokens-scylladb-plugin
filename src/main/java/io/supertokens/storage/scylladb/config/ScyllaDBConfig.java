3b314814404838d552036ecb0c4778c935193937
tax25 <andreatax25@gmail.com>
2023-04-14 16:57:57 +0200

    Added properties in ScyllaDBConfig.java and modified a method

--- src/main/java/io/supertokens/storage/scylladb/config/ScyllaDBConfig.java
+++ src/main/java/io/supertokens/storage/scylladb/config/ScyllaDBConfig.java
@@ -69,6 +69,124 @@ public class ScyllaDBConfig {
     @JsonProperty
     private int scylladb_port = null;
 
+    private final String USERS_TABLE = "all_auth_recipe_users";
+    private final String KEY_VALUE_TABLE = "key_value";
+    private final String USER_LAST_ACTIVE_TABLE = "user_last_active";
+    private final String ACCESS_TOKEN_SIGNIN_KEYS_TABLE = "session_access_token_signin_keys";
+    private final String SESSION_INFO_TABLE = "session_info";
+    private final String EMAIL_PASSWORD_USERS_TABLE = "emailpassword_users";
+    private final String PASSWORD_RESET_TOKENS_TABLE = "emailpassword_pswd_reset_token";
+    private final String EMAIL_VERIFICATION_TOKENS_TABLE = "emailverification_tokens";
+    private final String EMAIL_VERIFICATION_VERIFIED_EMAILS_TABLE = "emailverification_verified_emails";
+    private final String THIRD_PARTY_USERS_TABLE = "thirdparty_users";
+    private final String PASSWORDLESS_USERS_TABLE = "passwordless_users";
+    private final String PASSWORDLESS_DEVICES_TABLE = "passwordless_devices";
+    private final String PASSWORDLESS_CODES_TABLE = "passwordless_codes";
+    private final String JWT_SIGNIN_KEYS_TABLE = "jwt_signin_keys";
+    private final String USER_METADATA_TABLE = "user_metadata";
+    private final String ROLES_TABLE = "roles";
+    private final String USER_ROLES_PERMISSION_TABLE = "role_permission";
+    private final String USER_ROLES_TABLE = "user_roles";
+    private final String USER_ID_MAPPING_TABLE = "userid_mapping";
+    private final String DASHBOARD_USERS_TABLE = "dashboard_users";
+    private final String DASHBOARD_SESSIONS_TABLE = "dashboard_user_sessions";
+    private final String TOTP_USERS_TABLE = "totp_users";
+    private final String TOTP_USER_DEVICES_TABLE = "totp_user_devices";
+    private final String TOTP_USED_CODES_TABLE = "totp_used_codes";
+
+    // methods to get all table names
+    public String getUsersTable() {
+        return this.USERS_TABLE;
+    }
+
+    public String getKeyValueTable() {
+        return this.KEY_VALUE_TABLE;
+    }
+
+    public String getUserLastActiveTable() {
+        return this.USER_LAST_ACTIVE_TABLE;
+    }
+
+    public String getAccessTokenSigningKeys() {
+        return this.ACCESS_TOKEN_SIGNIN_KEYS_TABLE;
+    }
+
+    public String getSessionInfoTable(){
+        return this.SESSION_INFO_TABLE;
+    }
+
+    public String getEmailPasswordUsersTable() {
+        return this.EMAIL_PASSWORD_USERS_TABLE;
+    }
+
+    public String getPasswordResetTokensTable() {
+        return this.PASSWORD_RESET_TOKENS_TABLE;
+    }
+
+    public String getEmailVerificationTokensTable() {
+        return this.EMAIL_VERIFICATION_TOKENS_TABLE;
+    }
+
+    public String getEmailVerificationTable() {
+        return this.EMAIL_VERIFICATION_VERIFIED_EMAILS_TABLE;
+    }
+
+    public String getThirdPartyUsersTable() {
+        return this.THIRD_PARTY_USERS_TABLE;
+    }
+
+    public String getPasswordlessUsersTable() {
+        return this.PASSWORDLESS_USERS_TABLE;
+    }
+
+    public String getPasswordlessDevicesTable() {
+        return this.PASSWORDLESS_DEVICES_TABLE;
+    }
+
+    public String getPasswordlessCodesTable() {
+        return this.PASSWORDLESS_CODES_TABLE;
+    }
+
+    public String getUserMetadataTable() {
+        return this.USER_METADATA_TABLE;
+    }
+
+    public String getRolesTable() {
+        return this.ROLES_TABLE;
+    }
+
+    public String getUserRolesPermissionTable() {
+        return this.USER_ROLES_PERMISSION_TABLE;
+    }
+
+    public String getUserRolesTable() {
+        return this.USER_ROLES_TABLE;
+    }
+
+    public String getUserIdMappingTable() {
+        return this.USER_ID_MAPPING_TABLE;
+    }
+
+    public String getDashboardUsersTable() {
+        return this.DASHBOARD_USERS_TABLE;
+    }
+
+    public String getDashboardSessionsTable() {
+        return this.DASHBOARD_SESSIONS_TABLE;
+    }
+
+    public String getTotpUsersTable() {
+        return this.TOTP_USERS_TABLE;
+    }
+
+    public String getTotpUserDevicesTable() {
+        return this.TOTP_USER_DEVICES_TABLE;
+    }
+
+    public String getTotpUsedCodesTable() {
+        return this.TOTP_USED_CODES_TABLE;
+    }
+
     /*
     public int getConnectionPoolSize() {
         return postgresql_connection_pool_size;
@@ -158,172 +276,8 @@ public class ScyllaDBConfig {
         return postgresql_connection_uri;
     }
 
-    public String getUsersTable() {
-        return addSchemaAndPrefixToTableName("all_auth_recipe_users");
-    }
-
-    public String getKeyValueTable() {
-        String tableName = "key_value";
-        if (postgresql_key_value_table_name != null) {
-            return addSchemaToTableName(postgresql_key_value_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getUserLastActiveTable() {
-        return addSchemaAndPrefixToTableName("user_last_active");
-    }
-
-    public String getAccessTokenSigningKeysTable() {
-        return addSchemaAndPrefixToTableName("session_access_token_signing_keys");
-    }
-
-    public String getSessionInfoTable() {
-        String tableName = "session_info";
-        if (postgresql_session_info_table_name != null) {
-            return addSchemaToTableName(postgresql_session_info_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getEmailPasswordUsersTable() {
-        String tableName = "emailpassword_users";
-        if (postgresql_emailpassword_users_table_name != null) {
-            return addSchemaToTableName(postgresql_emailpassword_users_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getPasswordResetTokensTable() {
-        String tableName = "emailpassword_pswd_reset_tokens";
-        if (scylladb_emailpassword_pswd_reset_tokens_table_name != null) {
-            return addSchemaToTableName(scylladb_emailpassword_pswd_reset_tokens_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getEmailVerificationTokensTable() {
-        String tableName = "emailverification_tokens";
-        if (postgresql_emailverification_tokens_table_name != null) {
-            return addSchemaToTableName(postgresql_emailverification_tokens_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getEmailVerificationTable() {
-        String tableName = "emailverification_verified_emails";
-        if (postgresql_emailverification_verified_emails_table_name != null) {
-            return addSchemaToTableName(postgresql_emailverification_verified_emails_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getThirdPartyUsersTable() {
-        String tableName = "thirdparty_users";
-        if (postgresql_thirdparty_users_table_name != null) {
-            return addSchemaToTableName(postgresql_thirdparty_users_table_name);
-        }
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getPasswordlessUsersTable() {
-        String tableName = "passwordless_users";
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getPasswordlessDevicesTable() {
-        String tableName = "passwordless_devices";
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getPasswordlessCodesTable() {
-        String tableName = "passwordless_codes";
-        return addSchemaAndPrefixToTableName(tableName);
-    }
-
-    public String getJWTSigningKeysTable() {
-        return addSchemaAndPrefixToTableName("jwt_signing_keys");
-    }
-
-    public String getUserMetadataTable() {
-        return addSchemaAndPrefixToTableName("user_metadata");
-    }
-
-    public String getRolesTable() {
-        return addSchemaAndPrefixToTableName("roles");
-    }
-
-    public String getUserRolesPermissionsTable() {
-        return addSchemaAndPrefixToTableName("role_permissions");
-    }
-
-    public String getUserRolesTable() {
-        return addSchemaAndPrefixToTableName("user_roles");
-    }
-
-    public String getUserIdMappingTable() {
-        return addSchemaAndPrefixToTableName("userid_mapping");
-    }
-
-    public String getDashboardUsersTable() {
-        return addSchemaAndPrefixToTableName("dashboard_users");
-    }
-
-    public String getDashboardSessionsTable() {
-        return addSchemaAndPrefixToTableName("dashboard_user_sessions");
-    }
-
-    public String getTotpUsersTable() {
-        return addSchemaAndPrefixToTableName("totp_users");
-    }
-
-    public String getTotpUserDevicesTable() {
-        return addSchemaAndPrefixToTableName("totp_user_devices");
-    }
-
-    public String getTotpUsedCodesTable() {
-        return addSchemaAndPrefixToTableName("totp_used_codes");
-    }
-
-    private String addSchemaAndPrefixToTableName(String tableName) {
-        String name = tableName;
-        if (!postgresql_table_names_prefix.trim().equals("")) {
-            name = postgresql_table_names_prefix.trim() + "_" + name;
-        }
-        return addSchemaToTableName(name);
-    }
-
-    /*
-    private String addSchemaToTableName(String tableName) {
-        String name = tableName;
-        if (!postgresql_table_schema.trim().equals("public")) {
-            name = postgresql_table_schema.trim() + "." + name;
-        }
-        return name;
-    }
-    */
-
     void validateAndInitialise() {
-        if (postgresql_connection_uri != null) {
-            try {
-                URI ignored = URI.create(postgresql_connection_uri);
-            } catch (Exception e) {
-                throw new QuitProgramFromPluginException(
-                        "The provided postgresql connection URI has an incorrect format. Please use a format like "
-                                + "postgresql://[user[:[password]]@]host[:port][/dbname][?attr1=val1&attr2=val2...");
-            }
-        } else {
-            if (this.getUser() == null) {
-                throw new QuitProgramFromPluginException(
-                        "'postgresql_user' and 'postgresql_connection_uri' are not set. Please set at least one of "
-                                + "these values");
-            }
-        }
-
-        if (getConnectionPoolSize() <= 0) {
-            throw new QuitProgramFromPluginException(
-                    "'postgresql_connection_pool_size' in the config.yaml file must be > 0");
-        }
+        // this method has to check if all the mandatory parameters are inserted and valid
     }
 
 }
